0e0f437442b7067c4a57e2c7d743538848d23bf9
wizard-paso <wzpaso@gmail.com>
2015-09-21 02:17:04 +0900

    途中経過

--- 00_source/01_client/admin/app.js
+++ 00_source/01_client/admin/app.js
@@ -11,19 +11,19 @@
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
-                templateUrl: 'views/home.view.html',
+                templateUrl: 'home/home.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/login', {
                 controller: 'LoginController',
-                templateUrl: 'views/login.view.html',
+                templateUrl: 'login/login.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/register', {
                 controller: 'RegisterController',
-                templateUrl: 'views/register.view.html',
+                templateUrl: 'register/register.view.html',
                 controllerAs: 'vm'
             })
 
