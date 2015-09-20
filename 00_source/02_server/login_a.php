f47aae73fa38b73813a06eab597e84d29515eec9
koba58 <hiroshi.k20@gmail.com>
2015-09-21 04:10:37 +0900

    201509210410

--- 00_source/02_server/login_a.php
+++ 00_source/02_server/login_a.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-Type: text/html; charset=UTF-8");
 function login($userID, $password) {
 
     // file関数はファイル全体を読み込んで配列に格納する
@@ -13,10 +14,13 @@ function login($userID, $password) {
     return false;
 }
 
-$userID = $_POST["userID"];
-$password = $_POST["password"];
-var $a = login($userID,$password);
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+$userID = $data["userID"];
+$password = $data["password"];
+$a = login($userID, $password);
 //userIDはゆくゆくはuserNameに変更by kj
-$b = array('result' => $a, 'userID' => $userID);
-echo json_encode($b);
+$b = json_encode(array('result' => $a, 'userID' => $userID));
+//header('Content-Type: text/javascript; charset=utf-8');
+echo $b;
 ?>
