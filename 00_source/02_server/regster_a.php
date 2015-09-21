35c2c477db2e428616113739e2333a636d002395
koba58 <hiroshi.k20@gmail.com>
2015-09-22 01:06:53 +0900

    201509220106_register.php修正

--- 00_source/02_server/regster_a.php
+++ 00_source/02_server/regster_a.php
@@ -1,30 +1,24 @@
-<!DOCTYPE html>
-<head>
-<html lang="ja">
-<meta charset="UTF-8">
-<title>SmaQ - ログインページ</title>
-</head>
-<body>
 <?php
-    // 受け取った情報をfileに書き込む
-function write($firstname,$lastname,$userID, $password) {
-    $regster = "$firstname,$lastname,$userID, $password" . PHP_EOL;
-    file_put_contents("admin_user.csv.", $regster);
+// 受け取った情報をfileに書き込む
+function write($userID, $password) {
+    if($userID != "" && $password != ""){
+        $regster = "$userID, $password" . PHP_EOL;
+        file_put_contents("admin_user.csv", $regster, FILE_APPEND);
+        return true;
+    }
+    return false;
 }
 
 //受け取りうんぬんは後ほどアップデート予定 $_POST
 $json = file_get_contents("php://input");
 $data = json_decode($json, true);
-//ここまで
 
-$firstname = $data["firstname"];
-$lastname = $data["lastname"];
-$userID = $data["userID"];
-$password = $data["password"];
-$a = write($firstname,$lastname,$userID, $password);
-$b = json_encode(array('result' => $a, 'userID' => $userID));
-//header('Content-Type: text/javascript; charset=utf-8');
+$userID = $_POST["userID"];
+$password = $_POST["password"];
+$dm = " "
+$a = write($userID, $password);
+$b = json_encode(array('result' => $a, 'resultdesc' => $dm));
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
 echo $b;
 ?>
-</body>
-</html>
