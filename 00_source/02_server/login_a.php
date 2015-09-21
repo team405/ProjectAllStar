b3e4048aa6089e40b7f08b3f95bad3bf753c86ca
kenkoji11 <timeismoney11>
2015-09-22 00:01:14 +0900

    aaa
    aaa

--- 00_source/02_server/login_a.php
+++ 00_source/02_server/login_a.php
@@ -1,7 +1,14 @@
+<!DOCTYPE html>
+<head>
+<html lang="ja">
+<meta charset="UTF-8">
+<title>SmaQ - ログインページ</title>
+</head>
+<body>
 <?php
 function login($userID, $password) {
     // file関数はファイル全体を読み込んで配列に格納する
-    $lines = file("admin_user.csv", FILE_IGNORE_NEW_LINES);
+    $lines = file("user.csv", FILE_IGNORE_NEW_LINES);
     foreach ($lines as $line) {
         $user = explode(",", $line);
         if ($user[0] === $userID && $user[1] === $password) {
@@ -11,16 +18,21 @@ function login($userID, $password) {
     }
     return false;
 }
-//$json = file_get_contents("php://input");
-//$data = json_decode($json, true);
-//$userID = $data["userID"];
-//$password = $data["password"];
-$userID = $_POST["userID"]; //浅井追記
-$password = $_POST["password"];
+
+$dm = if(login($userID, $password)){
+  ""
+}else{
+  "fuck"
+};
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+$userID = $data["userID"];
+$password = $data["password"];
 $a = login($userID, $password);
 //userIDはゆくゆくはuserNameに変更by kj
-$b = json_encode(array('result' => $a, 'userID' => $userID));
-header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json; charset=utf-8');
+$b = json_encode(array('result' => $a, 'userID' => $userID, resultdesc => $dm));
+//header('Content-Type: text/javascript; charset=utf-8');
 echo $b;
 ?>
+</body>
+</html>
