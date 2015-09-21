206147420bd75a57602af4dbf4697a37e843d050
wizard-paso <wzpaso@gmail.com>
2015-09-22 02:08:18 +0900

    Revert "Revert "動くもの２""
    This reverts commit f34e07f58abc474b4c4fe518f5d9b1cee85851ac.

--- 00_source/01_client/admin/scripts/app.js
+++ 00_source/01_client/admin/scripts/app.js
@@ -33,6 +33,12 @@
                 controllerAs: 'vm'
             })
 
+            .when('/player', {
+                controller: 'PlayerController',
+                templateUrl: 'views/player.view.html',
+                controllerAs: 'vm'
+            })
+
             .otherwise({ redirectTo: '/login' });
     }
 
