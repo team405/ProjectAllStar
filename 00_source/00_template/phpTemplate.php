1caf53fa42821f41c00d5f80093558eabe87deb6
asian-tomato <asiantomato0129@gmail.com>
2015-09-22 00:34:45 +0900

    asai1509220034
    yoro

---
+++ 00_source/00_template/phpTemplate.php
@@ -0,0 +1,22 @@
+﻿<?php
+//処理内容は関数に！
+function sampleFunction($var1, $ver2,,,) {
+
+}
+
+//$POST_["name"]的に、普通にPOSTとして受け取れる！
+$var1 = $_POST[0];
+$var2 = $_POST[1];
+
+$dm = "dummy";
+
+//処理を実行！
+$a = sampleFuntion($var1, $var2,,,);
+//出力させるためのJSONをつくるよ
+$b = json_encode(array('userID' => $userID, 'result' => $a, 'resultDesc' => $dm));
+
+//おまじないを唱えて、echoする。
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+echo $b;
+?>
