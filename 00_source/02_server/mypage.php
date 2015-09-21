fd16ab1fbc10714fd4909242755eb79b02c75bee
kenkoji11 <timeismoney11>
2015-09-22 02:21:42 +0900

    20150922_0221
    mypage.php

---
+++ 00_source/02_server/mypage.php
@@ -0,0 +1,23 @@
+<?php
+function contents($userID) {
+    $lines = file("content.csv", FILE_IGNORE_NEW_LINES);
+    foreach ($lines as $line) {
+        $user = explode(",", $line);
+        if ($user[0] === $userID) {
+          $contents = array($user[1],$user[2]);
+          return array(true,$contents,"");
+        }
+    }
+    return array(false,"0","0","fuck");
+}
+
+$userID = $_POST["userID"]; //浅井追記
+foreach(contents($userID) as $line2){
+  $a = explode(",", $line2);
+  $b = json_encode(array('result' => $a[0], 'contentID' => $a[1],'contentName' => $a[2], 'resultdesc' => $a[3]));
+}
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+echo $b;
+?>
