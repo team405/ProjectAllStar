bc95ae2110bc15df1360a50b70b9293320befa5b
koba58 <hiroshi.k20@gmail.com>
2015-09-21 19:23:30 +0900

    ファイル名修正

--- 00_source/02_server/login_a.php
+++ 00_source/02_server/login_a.php
@@ -8,7 +8,7 @@
 <?php
 function login($userID, $password) {
     // file関数はファイル全体を読み込んで配列に格納する
-    $lines = file("user.csv", FILE_IGNORE_NEW_LINES);
+    $lines = file("admin_user.csv", FILE_IGNORE_NEW_LINES);
     foreach ($lines as $line) {
         $user = explode(",", $line);
         if ($user[0] === $userID && $user[1] === $password) {
