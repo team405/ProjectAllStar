1bcc5766d910c08ab9802b6fabb00e6b174ee0cb
koba58 <hiroshi.k20@gmail.com>
2015-09-23 01:06:23 +0900

    start.php追加
    start.php追加とques.phpの途中をコミット

---
+++ 00_source/02_server/start.php
@@ -0,0 +1,28 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] != "POST"){
+   $userID = $_GET["userID"];
+   $contentID = $_GET["contentID"];
+   $quesID = $_GET["quesID"];
+}else {
+   $userID = $_POST["userID"];
+   $contentID = $_POST["contentID"];
+   $quesID = $_POST["quesID"];
+}
+$path="";
+$result="false";
+$resultDesc="";
+
+if ($quesID !== "" && $contentID !== "" && $quesID !== "" ) {
+  $path = "data/".$userID.'/'.$contentID.'/'; 
+  $now = microtime(true);
+  $answer = $quesID. "," . $now. PHP_EOL;
+  file_put_contents($path."starttimestamp.csv", $answer, FILE_APPEND);
+  $result = "true";
+} else{
+  $resultDesc="fuck";
+}
+$b = json_encode(array('result' => $result,'resultDesc' => $resultDesc));
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+echo $b;
+?>
