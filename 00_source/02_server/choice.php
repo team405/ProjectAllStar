3d57e75597ccc0396906306843260da2e2c949b5
kenkoji11 <timeismoney11>
2015-09-22 16:55:03 +0900

    choice
    chochocho

---
+++ 00_source/02_server/choice.php
@@ -0,0 +1,31 @@
+<?php
+// 受け取った情報をfileに書き込む
+function write($userID) {
+    if($userID != ""){
+        $register = "$userID" . PHP_EOL;
+        file_put_contents("data/$userID/0/.csv", $register, FILE_APPEND);
+        return true;
+    }
+    return false;
+}
+
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+
+if($_SERVER["REQUEST_METHOD"] != "POST"){
+  $userID = $_GET["userID"];
+  $password = $_GET["password"];
+}else {
+  $userID = $_POST["userID"]; //浅井追記
+  $password = $_POST["password"];
+}
+
+//$userID = $_POST["userID"];
+//$password = $_POST["password"];
+$dm = "";
+$a = write($userID, $password);
+$b = json_encode(array('result' => $a, 'resultdesc' => $dm));
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+echo $b;
+?>
