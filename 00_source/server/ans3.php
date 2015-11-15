0618d75de2c785e6d8b8b41d8bc8eb5ee414bddd
kenkoji11 <timeismoney11>
2015-11-15 14:51:33 +0900

    ans途中のやつ

--- 00_source/server/ans3.php
+++ 00_source/server/ans3.php
@@ -38,29 +38,61 @@ $mysqli->close();
 }
 
 
+$mysqli = new mysqli("localhost", "dbsmaq", "ufbn516", "dbsmaq");
+if ($mysqli->connect_error) {
+    echo $mysqli->connect_error;
+    exit();
+}
 
+$link = mysqli_connect("localhost", "dbsmaq", "ufbn516", "dbsmaq");
 
+//ここに処理書くよ
+//startTimestamp,quessec,correctNum取得
+$sql = "SELECT * FROM question WHERE adminUid = '$userID' AND contentID = '$contentID' AND quesNum = '$quesID' ";
+if($sql_result = mysqli_query($link,$sql)){
+    while($row = mysqli_fetch_assoc($sql_result)){
+        $starttimestamp = $row['startTimeStamp'];
+        $quessec = $row['quesSec']
+        $correctNum = $row['correctNum'];
+        //array_push($quesArray,array( "preKind" => $row['preKind'],"preText" => $row['preText'],"quesText" => $row['quesText'],"choiceKind"=> $row['quesKind'],"choice"=>$choices,"quesSec"=>$row['quesSec'],"ansText"=>$corrects,"correctNumber"=>$row['correctNum']));
+        $result="true";
+}
+}else{
+    $resultDesc="error";
+}
+mysqli_free_result($sql_result);
+// 結果セットを閉じる
+// DB接続を閉じる
+mysqli_close($link);
+
+$quesEndsec = $starttimestamp + $quessec;
 $choice = array(0, 0, 0, 0);
 //各選択肢毎の人数。初期値は全部0
 
-if ($userID !== "" && $contentID !== "" && $quesID !== "" ) {
+$mysqli = new mysqli("localhost", "dbsmaq", "ufbn516", "dbsmaq");
+if ($mysqli->connect_error) {
+    echo $mysqli->connect_error;
+    exit();
+}
 
-  $filename = "data/".$userID.'/'.$contentID.'/'.$quesID.'/'.'config.ini';
-  $fileData = file_get_contents($filename);
-  $decode = json_decode($fileData, true);
-  $quesSec = $decode["quesSec"];
+$link = mysqli_connect("localhost", "dbsmaq", "ufbn516", "dbsmaq");
 
-  $path = "data/".$userID.'/'.$contentID.'/';
+//ここに処理書くよ
+$sql = "SELECT * FROM ansTime WHERE contentID = '$contentID' AND answeTimeStamp >= '$starttimestamp' AND answeTimeStamp =< '$quesEndsec' ";
+if($sql_result = mysqli_query($link,$sql)){
+    while($row = mysqli_fetch_assoc($sql_result)){
 
-  $getTimeStamp = 0.0;
-  $starts = file($path."starttimestamp.csv", FILE_IGNORE_NEW_LINES);
-  //タイムスタンプを取得する
-  foreach ($starts as $starttimestamp) {
-    $start_array = explode(",", $starttimestamp);
-    if($start_array[0] === $quesID){
-      $getTimeStamp = $start_array[1];
-    }
-  }
+
+        $result="true";
+}
+}else{
+    $resultDesc="error";
+}
+mysqli_free_result($sql_result);
+// 結果セットを閉じる
+// DB接続を閉じる
+mysqli_close($link);
+
   //回答を集計する
   $answers = file($path."answer.csv", FILE_IGNORE_NEW_LINES);
   foreach ($answers as $userans) {
@@ -79,6 +111,11 @@ if ($userID !== "" && $contentID !== "" && $quesID !== "" ) {
   $resultDesc="Error";
   $b = json_decode(array('result' => $result, 'resultDesc' => $resultDesc));
 }
+
+
+//$b = json_encode(array('preKind' => $preKind,'preText' => $preText,'quesText' => $quesText,'choiceKind'=> $choiceKind,'choiceText'=>$choices,'quesSec'=>$quesSec,'ansText'=>$corrects,'correctNumber'=>$correctNumber,'result' => $result, 'resultdesc' => $resultDesc,));
+
+
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
 echo $b;
