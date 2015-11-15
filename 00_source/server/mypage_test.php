e767acc1f5bc002b33302389bf28596be30e02c5
wizard-paso <wzpaso@gmail.com>
2015-11-15 12:50:39 +0900

    tesuto用追加

--- 00_source/server/mypage_test.php
+++ 00_source/server/mypage_test.php
@@ -1,5 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
-print '{"result":true,"resultdesc":"","contents":[{"contentID":0,"contentName":"event1"},{"contentID":1,"contentName":"event2"},{"contentID":2,"contentName":"event3"}]}'
+print '{"result":true,"resultdesc":"","contents":[{"contentID":0,"contentName":"event1","quesSum":3},{"contentID":1,"contentName":"event2","quesSum":3},{"contentID":2,"contentName":"event3","quesSum":3}]}'
 ?>
