569a8029bf78a95977941271056cb8e54d37f151
kenkoji11 <timeismoney11>
2015-11-22 23:16:47 +0900

    こんどこそ

--- 00_source/server/res.php
+++ 00_source/server/res.php
@@ -3,9 +3,9 @@
     //一字ファイルができているか（アップロードされているか）チェック
     if(is_uploaded_file($_FILES['up_file']['tmp_name'][0])){
 
-$path = "/../server/data/";
+$path = "../server/data/";
         //一字ファイルを保存ファイルにコピーできたか
-        if(move_uploaded_file($_FILES['up_file']['tmp_name'][0],$path.$_FILES["upfile"]["tmp_name"][0])){
+        if(move_uploaded_file($_FILES['up_file']['tmp_name'][0],$path . $_FILES["upfile"]["tmp_name"][0])){
 
             //正常
             echo "uploaded";
