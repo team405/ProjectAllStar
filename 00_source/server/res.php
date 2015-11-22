2870c1319e07fec78f9ec67a5d368fa587ebe19c
kenkoji11 <timeismoney11>
2015-11-22 20:53:23 +0900

    fuck

--- 00_source/server/res.php
+++ 00_source/server/res.php
@@ -1,11 +1,11 @@
 <?php
 
     //一字ファイルができているか（アップロードされているか）チェック
-    if(is_uploaded_file($_FILES['up_file']['tmp_name'])){
+    if(is_uploaded_file($_FILES['up_file']['tmp_name'][0])){
 
 $path = "/../server/data/";
         //一字ファイルを保存ファイルにコピーできたか
-        if(move_uploaded_file($_FILES['up_file']['tmp_name'],$path.$_FILES["upfile"]["tmp_name"])){
+        if(move_uploaded_file($_FILES['up_file']['tmp_name'][0],$path.$_FILES["upfile"]["tmp_name"][0])){
 
             //正常
             echo "uploaded";
